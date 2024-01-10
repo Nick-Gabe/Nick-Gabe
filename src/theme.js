bc3cce182cfc15aeaa489be37852debf9dae7aac
Bruno de Oliveira <jose.oliveira@encora.com>
2024-01-10 15:06:36 -0300

    Detect over colors automatically (#6)
    * adds css-color-converter package
    
    * adds utility function to get color brightness
    
    * adds logic to detect over colors automatically

--- src/theme.js
+++ src/theme.js
@@ -1,27 +1,23 @@
+const { getColorBrightness } = require("./utils/getColorBrightness");
+
 const black = '#121111';
 const white = '#F2F2F2';
 
+function generateBaseAndOverColor(color) {
+  const brightnessThreshold = 0.69;
+
+  return {
+    base: color,
+    over: getColorBrightness(color) <= brightnessThreshold ? white : black,
+  }
+}
+
 module.exports = {
   colors: {
-    primary: {
-      base: '#F73C7B',
-      over: white,
-    },
-    secondary: {
-      base: '#393359',
-      over: white,
-    },
-    lightSecondary: {
-      base: '#4D4577',
-      over: white,
-    },
-    black: {
-      base: black,
-      over: white,
-    },
-    white: {
-      base: white,
-      over: black,
-    },
+    primary: generateBaseAndOverColor('#F73C7B'),
+    secondary: generateBaseAndOverColor('#393359'),
+    lightSecondary: generateBaseAndOverColor('#4D4577'),
+    black: generateBaseAndOverColor(black),
+    white: generateBaseAndOverColor(white),
   },
 };
---
+++ src/utils/getColorBrightness.js
@@ -0,0 +1,18 @@
+const { fromString } = require('css-color-converter')
+
+function getColorBrightness(color) {
+  if (color) {
+    const cssColor = fromString(color)
+
+    if (cssColor) {
+      const rgb = cssColor.toRgbaArray()
+      return +((rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 255000).toFixed(2)
+    }
+  }
+
+  return 0
+}
+
+module.exports = {
+  getColorBrightness
+}
